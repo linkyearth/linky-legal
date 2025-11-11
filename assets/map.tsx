3cc2d8ebf123998e5873b47ddfa016be75b22584
yohaiaz <36672468+yohaiaz@users.noreply.github.com>
2025-11-11 11:25:16 +0200

    Update map.tsx

--- assets/map.tsx
+++ assets/map.tsx
@@ -3,22 +3,28 @@
 
   "themes": {
     "theme1": {
-      "background": "https://yourdomain.github.io/assets/backgrounds/map-glow-001.jpg",
-      "logo": "https://yourdomain.github.io/assets/logos/linky-logo-orange.png",
+      "background": "https://linkyearth.github.io/linky-legal/assets/bckg-1.png",
+      "logo": "https://linkyearth.github.io/linky-legal/assets/logo-1.png",
       "title": "Map your social universe",
       "subtitle": "Join the adventure! Pin your world, share your vibe, explore together."
     },
     "theme2": {
-      "background": "https://yourdomain.github.io/assets/backgrounds/map-chalk-002.jpg",
-      "logo": "https://yourdomain.github.io/assets/logos/linky-logo-chalk.png",
+      "background": "https://linkyearth.github.io/linky-legal/assets/bckg-2.png",
+      "logo": "https://linkyearth.github.io/linky-legal/assets/logo-2.png",
       "title": "Join the map of connections",
       "subtitle": "Discover and share your social world, one pin at a time."
     },
     "theme3": {
-      "background": "https://yourdomain.github.io/assets/backgrounds/map-chalk-002.jpg",
-      "logo": "https://yourdomain.github.io/assets/logos/linky-logo-chalk.png",
+      "background": "https://linkyearth.github.io/linky-legal/assets/bckg-3.png",
+      "logo": "https://linkyearth.github.io/linky-legal/assets/logo-3.png",
       "title": "Connect your socials to the world map",
       "subtitle": "Join the adventure!"
+    },
+    "theme4": {
+      "background": "https://linkyearth.github.io/linky-legal/assets/bckg-4.png",
+      "logo": "https://linkyearth.github.io/linky-legal/assets/logo-4.png",
+      "title": "Pin your world, share your vibe, explore together",
+      "subtitle": "Join this adventure! Let's re-invent together a more human social technology"
     }
   }
 }
